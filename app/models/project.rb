a8fae0c68c99485efcbe454e5015575b78cb8bcc
Champier Cyril <cyril.champier@doctolib.com>
2018-05-18 08:22:14 +0200

    add project color validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ApplicationRecord
   has_many :tasks
 
-  # TODO: validate color format: #FFFFFF
+  validates :color, presence: true, format: { with: /\A#[0-9A-F]{6}\z/, message: "should be a color 6 hexa (#FFFFFF)" }
+
 end
