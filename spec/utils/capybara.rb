abb9690fe482c4a4ec497ae1823e0231d07780b2
Champier Cyril <cyril.champier@doctolib.com>
2018-05-19 15:05:59 +0200

    travis: copy paste their doc

--- spec/utils/capybara.rb
+++ spec/utils/capybara.rb
@@ -1,4 +1,13 @@
 require 'capybara/rails'
 require 'capybara/rspec'
 
-Capybara.javascript_driver = :selenium_chrome
+# Capybara.javascript_driver = :selenium_chrome
+
+
+Capybara.register_driver :chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu])
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.javascript_driver = :chrome
