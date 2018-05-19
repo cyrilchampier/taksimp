25af612eac0b8e3b94178c03cdb007f7cca4d116
Champier Cyril <cyril.champier@doctolib.com>
2018-05-19 15:17:31 +0200

    travis: compile assets

--- spec/utils/capybara.rb
+++ spec/utils/capybara.rb
@@ -1,13 +1,11 @@
 require 'capybara/rails'
 require 'capybara/rspec'
 
+# TODO: not working on travis, should check why
 # Capybara.javascript_driver = :selenium_chrome
-
 
 Capybara.register_driver :chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu])
-
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
-
 Capybara.javascript_driver = :chrome
